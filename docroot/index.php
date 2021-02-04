75e73d70d8b1d1c255542a9f21adb2cff3f4dc8b
Laurie Lee Moses <MomoMoses@users.noreply.github.com>
2021-02-04 10:54:04 -0600

    Update index.php
    change photo, name, & description

--- docroot/index.php
+++ docroot/index.php
@@ -3,11 +3,11 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>UNCAP</title>
+<title>Portal</title>
 <?php include "includes/headstatic.php"; ?>
 <style type="text/css">
 .alert {
-background: #fef3d6; 
+background: #fef3d6;
 border: 1px solid #ccc;
 float: right;
 margin: 0 10px 10px 10px;
@@ -37,10 +37,10 @@ top: 3px;
 <!--TEXT-->
 <div id="textwrapper">
 
-<p><img alt="A tall stack of papers neatly organized into folders." id="homepagephoto" src="img/folders.jpg"/></p>
+<p><img alt="People in the street celebrating Juneteenth in Chicago" id="homepagephoto" src="img/Juneteenth_UChicagoNews_Prof Saville.png"/></p>
 
-<h2 id="about">UNCAP (Uncovering Chicago Archives Project) is a model for discovering primary sources across collections and institutions.</h2>
-
+<h2 id="about">The collections portal of the Black Metropolis Research Consortium highlights archival materials across Chicago archives, museums, and community
+organizations.</h2>
 <?php include "includes/description.html"; ?>
 
 <?php include "includes/footer.php"; ?>
